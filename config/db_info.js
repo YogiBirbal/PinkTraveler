a756a5f7b87d4c5e441b5afe16ca7dce072f404c
Yogi Birbal <31901286+YogiBirbal@users.noreply.github.com>
2018-11-06 12:30:13 -0500

    Add files via upload

---
+++ config/db_info.js
@@ -0,0 +1,11 @@
+module.exports = (function() {
+  return {
+    dev: {
+      host: 'localhost',
+      port: '8889',
+      user: 'root',
+      password: 'root',
+      database: 'cse305'
+    }
+  }
+})();
